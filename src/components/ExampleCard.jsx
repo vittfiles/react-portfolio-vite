b732381398f72d99767c20a8b77ecec2d49d8b5a
vittfiles <vittfiles@gmail.com>
2025-01-24 14:02:47 -0300

    se agregaron nuevos proyectos

--- src/components/ExampleCard.jsx
+++ src/components/ExampleCard.jsx
@@ -1,25 +1,52 @@
-import React from 'react';
+import React from "react";
 
-function ExampleCard({title, img, tags, info,index,demo,code}) {
-    let defClass = 'flex flex-col text-start p-4 my-4 bg-white rounded-lg w-full sm:w-[49%] md:w-[32%] sm:mr-[2%]';
-    if(index === 1){
-        defClass = 'flex flex-col text-start p-4 my-4 bg-white rounded-lg w-full sm:w-[49%] md:w-[32%] md:mr-[2%]';
-    }
-    if(index === 2){
-        defClass = 'flex flex-col text-start p-4 my-4 bg-white rounded-lg w-full sm:w-[49%] md:w-[32%]';
-    }
-    return ( <article className={defClass}>
-        <img className='rounded-lg aspect-[4/3] object-cover' src={img}/>
-        <ul className='font-medium text-base text-black-400 pt-4 flex flex-wrap'>
-            {tags.map(el => <li className='inline pr-2' key={el}>#{el}</li>)}
-        </ul>
-        <h2 className='pt-4 pb-2 text-black-400 font-semibold text-2xl mt-auto'>{title}</h2>
-        <p className='text-black-200 font-medium text-base pb-4'>{info}</p>
-        <div className='py-4'>
-            <a target="_blank" rel="noreferrer noopener" href={demo} className='font-medium text-lg text-white px-7 py-1 border-2 border-blues rounded-xl bg-blues mr-2 hover:text-blues hover:bg-white'>Demo</a>
-            <a target="_blank" rel="noreferrer noopener" href={code} className='font-medium text-lg text-blues px-6 py-1 border-2 border-blues rounded-xl hover:text-white hover:bg-blues'>Code</a>
-        </div>
-    </article> );
+function ExampleCard({ title, img, tags, info, index, demo, code }) {
+  let defClass =
+    "flex flex-col text-start p-4 my-4 bg-white rounded-lg w-full sm:w-[49%] md:w-[32%] sm:mr-[2%]";
+  if (index === 1) {
+    defClass =
+      "flex flex-col text-start p-4 my-4 bg-white rounded-lg w-full sm:w-[49%] md:w-[32%] md:mr-[2%]";
+  }
+  if (index === 2) {
+    defClass =
+      "flex flex-col text-start p-4 my-4 bg-white rounded-lg w-full sm:w-[49%] md:w-[32%]";
+  }
+  return (
+    <article className={defClass}>
+      <img className="rounded-lg aspect-[4/3] object-cover" src={img} />
+      <ul className="font-medium text-base text-black-400 pt-4 flex flex-wrap">
+        {tags.map((el) => (
+          <li className="inline pr-2" key={el}>
+            #{el}
+          </li>
+        ))}
+      </ul>
+      <h2 className="pt-4 pb-2 text-black-400 font-semibold text-2xl mt-auto">
+        {title}
+      </h2>
+      <p className="text-black-200 font-medium text-base pb-4">{info}</p>
+      <div className="py-4">
+        {demo && (
+          <a
+            target="_blank"
+            rel="noreferrer noopener"
+            href={demo}
+            className="font-medium text-lg text-white px-7 py-1 border-2 border-blues rounded-xl bg-blues mr-2 hover:text-blues hover:bg-white"
+          >
+            Demo
+          </a>
+        )}
+        <a
+          target="_blank"
+          rel="noreferrer noopener"
+          href={code}
+          className="font-medium text-lg text-blues px-6 py-1 border-2 border-blues rounded-xl hover:text-white hover:bg-blues"
+        >
+          Code
+        </a>
+      </div>
+    </article>
+  );
 }
 
 export default ExampleCard;
--- src/db/data.js
+++ src/db/data.js
@@ -1,138 +1,185 @@
 export const proj = [
-    {
-        id: 'responsive',
-        title: 'Responsive',
-        ej: [
-            {
-                title: 'Stripe store',
-                img: 'img/stripe-store-api-product-pc.webp',
-                tags: ['HTML', 'CSS', 'Javascript'],
-                info: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-                demo: "https://vittfiles.github.io/stripe-api-store",
-                code: "https://github.com/vittfiles/stripe-api-store"
-            },
-            {
-                title: 'CV design',
-                img: 'img/cv-design-pc.webp',
-                tags: ['HTML', 'CSS', 'Javascript'],
-                info: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-                demo: "https://vittfiles.github.io/proyecto-final",
-                code: "https://github.com/vittfiles/proyecto-final"
-            },
-            {
-                title: 'Recipe Blog',
-                img: 'img/blog-pc.webp',
-                tags: ['HTML', 'CSS'],
-                info: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-                demo: "https://vittfiles.github.io/recipe-page-vittfiles/",
-                code: "https://github.com/vittfiles/recipe-page-vittfiles"
-            },
-            {
-                title: 'My Gallery',
-                img: 'img/my-gallery.webp',
-                tags: ['HTML', 'CSS'],
-                info: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-                demo: "https://vittfiles.github.io/gallery-social",
-                code: "https://github.com/vittfiles/gallery-social"
-            },
-            {
-                title: 'Checkout',
-                img: 'img/checkout-pc.webp',
-                tags: ['HTML', 'CSS', 'Javascript'],
-                info: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-                demo: "https://vittfiles.github.io/buy-page/",
-                code: "https://github.com/vittfiles/buy-page"
-            },
-            {
-                title: 'Consultant',
-                img: 'img/consultant-pc.webp',
-                tags: ['HTML', 'CSS', 'Javascript'],
-                info: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-                demo: "https://vittfiles.github.io/interior-consultant-master",
-                code: "https://github.com/vittfiles/interior-consultant-master"
-            },
-            {
-                title: 'Team page',
-                img: 'img/team-page-pc.webp',
-                tags: ['HTML', 'CSS'],
-                info: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-                demo: "https://vittfiles.github.io/my-team-page-master",
-                code: "https://github.com/vittfiles/my-team-page-master"
-            },
-            {
-                title: '404 not found',
-                img: 'img/404-not-found.jpg',
-                tags: ['HTML', 'CSS', 'Javascript'],
-                info: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-                demo: "https://vittfiles.github.io/404-not-found-master/",
-                code: "https://github.com/vittfiles/404-not-found-master"
-            },
+  {
+    id: "java",
+    title: "Java",
+    ej: [
+      {
+        title: "Challenge Foro Hub",
+        img: "img/forohub.jpg",
+        tags: ["Java", "Spring Boot", "MySQL"],
+        info: "This is a challenge for the Oracle Next Education course made with Spring Boot",
+        code: "https://github.com/vittfiles/Alura-Oracle-Challenge-java-Spring-API-REST",
+      },
+      {
+        title: "Challenge Literalura",
+        img: "img/badge literalura.jpeg",
+        tags: ["Java", "Spring Boot", "PostgreSQL"],
+        info: "This is a challenge for the Oracle Next Education course made with Spring Boot",
+        code: "https://github.com/vittfiles/Alura-Oracle-Challenge-java-Spring",
+      },
+      {
+        title: "Challenge Conversor de Monedas",
+        img: "img/Badge-Conversor.jpeg",
+        tags: ["Java"],
+        info: "This is a challenge for the Oracle Next Education course",
+        code: "https://github.com/vittfiles/Alura-Oracle-Challenge-java-oop",
+      },
+    ],
+  },
+  {
+    id: "laravel",
+    title: "Laravel",
+    ej: [
+      {
+        title: "No Country RRHH",
+        img: "img/rrhhnocountry.jpeg",
+        tags: [
+          "Laravel",
+          "Next.js",
+          "PHP",
+          "Tailwind",
+          "MySQL",
+          "TypeScrypt",
+          "Responsive",
         ],
-        
-    },
-    {
-        id: 'angular',
-        title: 'Angular',
-        ej: [
-            {
-                title: 'Social network',
-                img: 'img/social-network.jpeg',
-                tags: ['Angular', 'Tailwind', 'Typescript', 'responsive'],
-                info: 'A social media web designed to upload images and comments, create a fake user and try it',
-                demo: "https://vittfiles.github.io/angular-social-network/",
-                code: "https://github.com/vittfiles/angular-social-network/"
-            },
-            {
-                title: 'Yugioh FMR',
-                img: 'img/yugiohfmr.jpeg',
-                tags: ['Angular', 'SASS', 'Typescript', 'responsive'],
-                info: 'In this project, I work with Angular to create a deck creator for Yugioh Forbidden memories',
-                demo: "https://vittfiles.github.io/yugiohfmr/",
-                code: "https://github.com/vittfiles/yugiohfmr"
-            },
-        ],
-    },
-    {
-        id: 'react',
-        title: 'React',
-        ej: [
-            {
-                title: 'Cuevanarg',
-                img: 'img/cuevanarg.webp',
-                tags: ['React', 'Tailwind', 'Javascript', 'responsive','API'],
-                info: 'In this project, I work with React and Tailwind to show movie data using the API The Movie Database',
-                demo: "https://vittfiles.github.io/alkemy-challenge-react/",
-                code: "https://github.com/vittfiles/alkemy-challenge-react"
-            },
-            {
-                title: 'Portfolio',
-                img: 'img/portfolio.jpeg',
-                tags: ['React', 'Tailwind', 'Javascript', 'responsive'],
-                info: 'In this project, I work with React and Tailwind to create a responsive prtfolio. The design is from devchallenge.io',
-                demo: "https://vittfiles.github.io/react-portfolio-vite/",
-                code: "https://github.com/vittfiles/react-portfolio-vite"
-            },
-            {
-                title: 'Crud',
-                img: 'img/crud-react.jpeg',
-                tags: ['React', 'Bootstrap 5', 'Javascript', 'responsive'],
-                info: 'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
-                demo: "https://vittfiles.github.io/react-crud",
-                code: "https://github.com/vittfiles/react-crud"
-            },
-        ],
-    },
-    {
-        id: 'wordpress',
-        title: 'Wordpress',
-        ej: [
-            {
-                title: 'Attribute Hierarchy',
-                img: 'img/attribute.webp',
-                tags: ['PHP', 'Wordpress', 'CSS', 'Javascript', 'MySQL'],
-                info: 'Vitt Attribute Hierarchy for WooCommerce is a wordpress plugin to simulate a hierarchy for attributes and subattributes.',
-                code: "https://github.com/vittfiles/vitt-attribute-hierarchy"
-            }
-        ],
-    },
+        info: "Web service where a company can manage its employees' vacations, all with end-to-end encription",
+        code: "https://github.com/vittfiles/No-Country-RRHH",
+      },
+    ],
+  },
+  {
+    id: "angular",
+    title: "Angular",
+    ej: [
+      {
+        title: "Social network",
+        img: "img/social-network.jpeg",
+        tags: ["Angular", "Tailwind", "Typescript", "responsive"],
+        info: "A social media web designed to upload images and comments, create a fake user and try it",
+        demo: "https://vittfiles.github.io/angular-social-network/",
+        code: "https://github.com/vittfiles/angular-social-network/",
+      },
+      {
+        title: "Yugioh FMR",
+        img: "img/yugiohfmr.jpeg",
+        tags: ["Angular", "SASS", "Typescript", "responsive"],
+        info: "In this project, I work with Angular to create a deck creator for Yugioh Forbidden memories",
+        demo: "https://vittfiles.github.io/yugiohfmr/",
+        code: "https://github.com/vittfiles/yugiohfmr",
+      },
+    ],
+  },
+  {
+    id: "react",
+    title: "React",
+    ej: [
+      {
+        title: "Cuevanarg",
+        img: "img/cuevanarg.webp",
+        tags: ["React", "Tailwind", "Javascript", "responsive", "API"],
+        info: "In this project, I work with React and Tailwind to show movie data using the API The Movie Database",
+        demo: "https://vittfiles.github.io/alkemy-challenge-react/",
+        code: "https://github.com/vittfiles/alkemy-challenge-react",
+      },
+      {
+        title: "Portfolio",
+        img: "img/portfolio.jpeg",
+        tags: ["React", "Tailwind", "Javascript", "responsive"],
+        info: "In this project, I work with React and Tailwind to create a responsive prtfolio. The design is from devchallenge.io",
+        demo: "https://vittfiles.github.io/react-portfolio-vite/",
+        code: "https://github.com/vittfiles/react-portfolio-vite",
+      },
+      {
+        title: "Crud",
+        img: "img/crud-react.jpeg",
+        tags: ["React", "Bootstrap 5", "Javascript", "responsive"],
+        info: "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+        demo: "https://vittfiles.github.io/react-crud",
+        code: "https://github.com/vittfiles/react-crud",
+      },
+    ],
+  },
+  {
+    id: "wordpress",
+    title: "Wordpress",
+    ej: [
+      {
+        title: "Attribute Hierarchy",
+        img: "img/attribute.webp",
+        tags: ["PHP", "Wordpress", "CSS", "Javascript", "MySQL"],
+        info: "Vitt Attribute Hierarchy for WooCommerce is a wordpress plugin to simulate a hierarchy for attributes and subattributes.",
+        code: "https://github.com/vittfiles/vitt-attribute-hierarchy",
+      },
+    ],
+  },
+  {
+    id: "responsive",
+    title: "Responsive",
+    ej: [
+      {
+        title: "Stripe store",
+        img: "img/stripe-store-api-product-pc.webp",
+        tags: ["HTML", "CSS", "Javascript"],
+        info: "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+        demo: "https://vittfiles.github.io/stripe-api-store",
+        code: "https://github.com/vittfiles/stripe-api-store",
+      },
+      {
+        title: "CV design",
+        img: "img/cv-design-pc.webp",
+        tags: ["HTML", "CSS", "Javascript"],
+        info: "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+        demo: "https://vittfiles.github.io/proyecto-final",
+        code: "https://github.com/vittfiles/proyecto-final",
+      },
+      {
+        title: "Recipe Blog",
+        img: "img/blog-pc.webp",
+        tags: ["HTML", "CSS"],
+        info: "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+        demo: "https://vittfiles.github.io/recipe-page-vittfiles/",
+        code: "https://github.com/vittfiles/recipe-page-vittfiles",
+      },
+      {
+        title: "My Gallery",
+        img: "img/my-gallery.webp",
+        tags: ["HTML", "CSS"],
+        info: "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+        demo: "https://vittfiles.github.io/gallery-social",
+        code: "https://github.com/vittfiles/gallery-social",
+      },
+      {
+        title: "Checkout",
+        img: "img/checkout-pc.webp",
+        tags: ["HTML", "CSS", "Javascript"],
+        info: "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+        demo: "https://vittfiles.github.io/buy-page/",
+        code: "https://github.com/vittfiles/buy-page",
+      },
+      {
+        title: "Consultant",
+        img: "img/consultant-pc.webp",
+        tags: ["HTML", "CSS", "Javascript"],
+        info: "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+        demo: "https://vittfiles.github.io/interior-consultant-master",
+        code: "https://github.com/vittfiles/interior-consultant-master",
+      },
+      {
+        title: "Team page",
+        img: "img/team-page-pc.webp",
+        tags: ["HTML", "CSS"],
+        info: "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+        demo: "https://vittfiles.github.io/my-team-page-master",
+        code: "https://github.com/vittfiles/my-team-page-master",
+      },
+      {
+        title: "404 not found",
+        img: "img/404-not-found.jpg",
+        tags: ["HTML", "CSS", "Javascript"],
+        info: "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+        demo: "https://vittfiles.github.io/404-not-found-master/",
+        code: "https://github.com/vittfiles/404-not-found-master",
+      },
+    ],
+  },
 ];
